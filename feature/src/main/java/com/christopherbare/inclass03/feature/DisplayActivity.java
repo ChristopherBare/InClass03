cd00666e3d299a2bbae62b0d761a6df9b6b493a4
christopher <christopherbare@outlook.com>
2018-06-06 05:19:34 -0400

    Setup onClick listeners for edit buttons and create new activity.

--- feature/src/main/java/com/christopherbare/inclass03/feature/DisplayActivity.java
+++ feature/src/main/java/com/christopherbare/inclass03/feature/DisplayActivity.java
@@ -1,12 +1,18 @@
 package com.christopherbare.inclass03.feature;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class DisplayActivity extends AppCompatActivity {
+    final static String STUDENT_KEY = "STUDENT";
+    Student passStudent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,7 @@ public class DisplayActivity extends AppCompatActivity {
 
         if (getIntent() != null && getIntent().getExtras() != null) {
             Student student = (Student) getIntent().getExtras().getSerializable(MainActivity.STUDENT_KEY);
+            passStudent = student;
 
             final TextView name = (TextView) findViewById(R.id.textView);
             final TextView email = (TextView) findViewById(R.id.textView4);
@@ -33,6 +40,49 @@ public class DisplayActivity extends AppCompatActivity {
                 department.setText("Department: " + departmentString);
                 mood.setText("Mood: " + moodString);
             }
+
         }
+
+        ImageButton editName = (ImageButton) findViewById(R.id.imageButton3);
+        ImageButton editEmail = (ImageButton) findViewById(R.id.imageButton4);
+        ImageButton editDepartment = (ImageButton) findViewById(R.id.imageButton5);
+        ImageButton editMood = (ImageButton) findViewById(R.id.imageButton6);
+
+        editName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DisplayActivity.this, EditActivity.class);
+                intent.putExtra(STUDENT_KEY, passStudent);
+                startActivity(intent);
+            }
+        });
+
+        editEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DisplayActivity.this, EditActivity.class);
+                intent.putExtra(STUDENT_KEY, passStudent);
+                startActivity(intent);
+            }
+        });
+
+        editDepartment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DisplayActivity.this, EditActivity.class);
+                intent.putExtra(STUDENT_KEY, passStudent);
+                startActivity(intent);
+            }
+        });
+
+        editMood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DisplayActivity.this, EditActivity.class);
+                intent.putExtra(STUDENT_KEY, passStudent);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ feature/src/main/java/com/christopherbare/inclass03/feature/EditActivity.java
@@ -0,0 +1,13 @@
+package com.christopherbare.inclass03.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
